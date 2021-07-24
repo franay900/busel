5a7c5f54d7aa46ff438c31c33158d47729239191
unknown <ilnur.bariev.2005@mail.ru>
2021-07-24 18:55:51 +0300

    busel_rc3.01

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,8 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 
 try:
-    from .local_settings import *
+    from .prod_setting import *
     
 except ImportError:
-    from .prod_setting  import *
+    from .local_settings  import *
 
