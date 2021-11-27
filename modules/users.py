a31e8c8ea808eb86b599a52ab0a5d47a39ddc086
unknown <ilnur.bariev.2005@mail.ru>
2021-11-27 23:29:33 +0300

    busel_rc4.02

--- modules/users.py
+++ modules/users.py
@@ -4,7 +4,12 @@ import random
 def get_user(file):
 	students=openpyxl.open(file,read_only=True)
 	sheet=students.active
-
+    ws = students.Sheets(sheet)
+    begrow = 1
+    endrow = ws.UsedRange.Rows.Count
+    for row in range(begrow,endrow+1): # just an example
+        if ws.Range('A{}'.format(row)).Value is None:
+            ws.Range('A{}'.format(row)).EntireRow.Delete(Shift=-4162) # shift up
 
 	return sheet
 
