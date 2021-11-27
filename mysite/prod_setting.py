b75ec6ce489489f2f8f18d2f4b0709c888e60495
unknown <ilnur.bariev.2005@mail.ru>
2021-11-27 21:12:17 +0300

    busel_rc4.02

--- mysite/prod_setting.py
+++ mysite/prod_setting.py
@@ -22,7 +22,7 @@ DATABASES = {
 
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'busel',
-        'USER' : 'admin_busel',
+        'USER' : 'busel_admin',
         'PASSWORD' : 'masterAdminBusel#345',
         'HOST' : 'localhost',
         'PORT' : '5432',
