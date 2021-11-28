b115d5c2e0c731eea77db3121d75be5b6ef57a52
unknown <ilnur.bariev.2005@mail.ru>
2021-11-28 12:53:05 +0300

    busel_rc4.3.2

--- modules/users.py
+++ modules/users.py
@@ -6,14 +6,9 @@ def get_user(file):
     students = openpyxl.open(file, read_only=True)
     sheet = students.active
 
-    ws = students.Sheets(sheet)
-    begrow = 1
-    endrow = ws.UsedRange.Rows.Count
-    for row in range(begrow, endrow + 1):  # just an example
-        if ws.Range('A{}'.format(row)).Value is None:
-            ws.Range('A{}'.format(row)).EntireRow.Delete(Shift=-4162)  # shift up
 
-        return sheet
+
+    return sheet
 
 
 def generate_login():
