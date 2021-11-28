5e6d1562fbceba4ba836fca5ca2e5227743d687f
unknown <ilnur.bariev.2005@mail.ru>
2021-11-28 12:32:41 +0300

    busel_rc4.3.1

--- modules/users.py
+++ modules/users.py
@@ -1,24 +1,27 @@
 import openpyxl
 import random
 
+
 def get_user(file):
-	students=openpyxl.open(file,read_only=True)
-	sheet=students.active
+    students = openpyxl.open(file, read_only=True)
+    sheet = students.active
+
     ws = students.Sheets(sheet)
     begrow = 1
     endrow = ws.UsedRange.Rows.Count
-    for row in range(begrow,endrow+1): # just an example
+    for row in range(begrow, endrow + 1):  # just an example
         if ws.Range('A{}'.format(row)).Value is None:
-            ws.Range('A{}'.format(row)).EntireRow.Delete(Shift=-4162) # shift up
+            ws.Range('A{}'.format(row)).EntireRow.Delete(Shift=-4162)  # shift up
 
-	return sheet
+        return sheet
+
 
 def generate_login():
     chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-    password=''
-    login=''
+    password = ''
+    login = ''
     for i in range(8):
         password += random.choice(chars)
     for i in range(8):
         login += random.choice(chars)
-    return login,password
+    return login, password
