7633f86ca7e737df10c4eeb41bd6768a5f4c2cf5
unknown <ilnur.bariev.2005@mail.ru>
2021-11-28 18:08:21 +0300

    busel_rc4.3.4

--- mysite/prod_setting.py
+++ mysite/prod_setting.py
@@ -1,30 +1,30 @@
 import os
 from pathlib import Path
 
-
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = '84xz44(&t+k=#dkwpdn_z92x8e07p4r07&x^0j_@sv1mhg54_&'
-DEBUG = True
 
-ALLOWED_HOSTS = []
+SECRET_KEY = '84xz4~A15HrFd*B5ec???9IetrvZXZq~ELH*UP5T4ACT0~La{Xou'
+DEBUG = False
+
+
 
 
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'mysite/static')
+    os.path.join(BASE_DIR,'/static')
 
 ]
 
 DATABASES = {
     'default': {
+        #
 
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'busel',
-        'USER' : 'sadmin',
-        'PASSWORD' : 'asszxx228',
-        'HOST' : '127.0.0.1',
+        'USER' : 'busel_admin',
+        'PASSWORD' : 'masterAdminBusel#345',
+        'HOST' : 'localhost',
         'PORT' : '5432',
     }
 }
