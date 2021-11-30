518ba247968ea0303c8915129fadad56135f61ad
unknown <ilnur.bariev.2005@mail.ru>
2021-12-01 00:00:45 +0300

    busel_rc4.3.1

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,11 +11,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+
 import os
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+ALLOWED_HOSTS = ['127.0.0.1','147.78.65.166','busel.ga']
+
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '84xz44(&t+k=#dkwpdn_z92x8e07p4r07&x^0j_@sv1mhg54_&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
     'institutions',
     'classes',
     'journal',
+    'reports',
 ]
 
 MIDDLEWARE = [
@@ -80,18 +82,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        #
 
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'busel',
-        'USER' : 'sadmin',
-        'PASSWORD' : 'asszxx228',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432',
-    }
-}
 
 
 # Password validation
@@ -127,17 +118,26 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
 
 STATIC_URL = '/static/'
-
+# STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 
 ]
+
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+
 AUTH_USER_MODEL = 'user_account.UserNet'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+
+try:
+    from .local_setting import *
+    
+except ImportError:
+    from .prod_setting  import *
+
