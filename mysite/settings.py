089da5ebb5f1573f47fefce7b72c72f69da695d8
ilnur <ilnur.bariev.2005@mail.ru>
2022-03-16 22:50:16 +0300

    busel_1.01

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '84xz44(&t+k=#dkwpdn_z92x8e07p4r07&x^0j_@sv1mhg54_&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 
