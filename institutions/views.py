601fec34b3baac9d8ca1671325888cd1c19cf03b
ilnur <ilnur.bariev.2005@mail.ru>
2022-07-10 02:06:13 +0300

    busel_1.3.5

--- institutions/views.py
+++ institutions/views.py
@@ -277,7 +277,7 @@ class InstitutionCreate(PermissionRequiredMixin, SuccessMessageMixin, CreateView
 
         institutions_create = form.save(commit=True)
         if self.request.user.is_superuser == False:
-            institutions_create.departmental_organization.set(self.request.user.institution)
+            institutions_create.departmental_organization = self.request.user.institution
             department_institution = Institutions.objects.get(pk=self.request.user.institution.pk)
             lim = department_institution.lim-1
             department_institution.save()
@@ -290,7 +290,6 @@ class InstitutionCreate(PermissionRequiredMixin, SuccessMessageMixin, CreateView
         user = UserNet.objects.create_user(username=self.login, password=self.password,
                                            institution=institutions_create)
         user.groups.set([group])
-        if self.request.user.is_superuser == False:
         return super().form_valid(form)
 
     def query(self):
